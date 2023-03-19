71809f56b3b663a4c0ec5accc236bab33700664a
Jensen Hwa <30474284+jensenhwa@users.noreply.github.com>
2023-03-19 12:14:20 -0700

    Add none model config; reduce edge model epochs

--- notepredictor.py
+++ notepredictor.py
@@ -15,11 +15,12 @@ import sys
 sweep_configuration = {
     'method': 'random',
     'name': 'sweep',
-    'metric': {'goal': 'maximize', 'name': 'val_acc'},
+    'metric': {'goal': 'maximize', 'name': 'link_val_acc'},
     'parameters':
     {
         'lr': {'values': [1e-3, 1e-4, 1e-5]},
-        'mlp_lr': {'values': [1e-2]},
+        'mlp_lr': {'values': [1e-2, 1e-4]},
+        'link_lr': {'values': [1e-2, 1e-4]},
         'num_layers': {'values': [2, 3, 4]},
         'hidden_dim': {'values': [128, 256, 512]},
         'output_dim': {'values': [128, 256, 512]},
@@ -37,57 +38,61 @@ sweep_configuration = {
 }
 
 
-def train():
-    wandb.init()
+def train(args=None):
+    if args:
+        wandb.init(**args)
+    else:
+        wandb.init()
     print(wandb.config)
+    wandb_logger = WandbLogger(project='note-predictor')
 
     try:
         train, val, graph = dataloader.get_datasets()
-        val_dataloader = dataloader.get_val_dataloader(val, wandb.config.model_type)
-        train_dataloader = dataloader.get_dataloader(train,
-                                                     wandb.config.walk_length,
-                                                     wandb.config.context_size,
-                                                     wandb.config.model_type)
-        wandb_logger = WandbLogger(project='note-predictor')
-        wandb_logger.experiment.config["hidden_dim"] = wandb.config.hidden_dim
-        wandb_logger.experiment.config["output_dim"] = wandb.config.output_dim
-        wandb_logger.experiment.config["num_layers"] = wandb.config.num_layers
 
-        # Learn node embeddings
-        if wandb.config.model_type == 'attri2vec':
-            node_model = Attri2Vec(train.x.shape[1], wandb.config.hidden_dim, wandb.config.output_dim,
-                              wandb.config.num_layers, wandb.config.lr)
-        elif wandb.config.model_type == 'gnn':
-            node_model = GNN(train.x.shape[1], wandb.config.hidden_dim, wandb.config.output_dim,
-                              wandb.config.num_layers, wandb.config.lr, train, val)
+        if wandb.config.model_type != 'none':
+            val_dataloader = dataloader.get_val_dataloader(val, wandb.config.model_type)
+            train_dataloader = dataloader.get_dataloader(train,
+                                                         wandb.config.walk_length,
+                                                         wandb.config.context_size,
+                                                         wandb.config.model_type)
+
+            # Learn node embeddings
+            if wandb.config.model_type == 'attri2vec':
+                node_model = Attri2Vec(train.x.shape[1], wandb.config.hidden_dim, wandb.config.output_dim,
+                                  wandb.config.num_layers, wandb.config.lr)
+            elif wandb.config.model_type == 'gnn':
+                node_model = GNN(train.x.shape[1], wandb.config.hidden_dim, wandb.config.output_dim,
+                                  wandb.config.num_layers, wandb.config.lr, train, val)
+            else:
+                raise ValueError('invalid model_type in wandb config')
+            wandb.watch(node_model, log="all", log_freq=1)
+            if torch.cuda.is_available():
+                trainer = pl.Trainer(max_epochs=50, accelerator="gpu", logger=wandb_logger)
+            else:
+                trainer = pl.Trainer(max_epochs=6, logger=wandb_logger)
+
+            trainer.fit(node_model,
+                        train_dataloaders=train_dataloader,
+                        val_dataloaders=val_dataloader)
+
+            # Get node embedding model
+            if wandb.config.model_type == 'attri2vec':
+                link_model = node_model.model
+            elif wandb.config.model_type == 'gnn':
+                mlp_model = train_gnn_mlp(node_model, wandb_logger, train.x.shape[1], wandb.config.hidden_dim, wandb.config.output_dim,
+                                  wandb.config.num_layers, wandb.config.mlp_lr, train, val)
+                link_model = mlp_model.model
+            else:
+                raise ValueError('invalid model_type in wandb config')
+
+            # Link prediction on learned node embeddings
+            embedded_test_graph = create_embedded_graph(graph, link_model)
         else:
-            raise ValueError('invalid model_type in wandb config')
-        wandb.watch(node_model, log="all", log_freq=1)
-        if torch.cuda.is_available():
-            trainer = pl.Trainer(max_epochs=50, accelerator="gpu", logger=wandb_logger)
-        else:
-            trainer = pl.Trainer(max_epochs=6, logger=wandb_logger)
-
-        trainer.fit(node_model,
-                    train_dataloaders=train_dataloader,
-                    val_dataloaders=val_dataloader)
-
-        # Get node embedding model
-        if wandb.config.model_type == 'attri2vec':
-            link_model = node_model.model
-        elif wandb.config.model_type == 'gnn':
-            mlp_model = train_gnn_mlp(node_model, wandb_logger, train.x.shape[1], wandb.config.hidden_dim, wandb.config.output_dim,
-                              wandb.config.num_layers, wandb.config.mlp_lr, train, val)
-            link_model = mlp_model.model
-        else:
-            raise ValueError('invalid model_type in wandb config')
-
-        # Link prediction on learned node embeddings
-        embedded_test_graph = create_embedded_graph(graph, link_model)
-        edge_model = EdgeLogisticRegression(wandb.config.output_dim, wandb.config.lr)
+            embedded_test_graph = dataloader.convert_graph_to_data(graph)
+        edge_model = EdgeLogisticRegression(wandb.config.output_dim, wandb.config.link_lr)
         in_sample_edge_dataloader, out_sample_edge_dataloader = get_edge_dataloader(embedded_test_graph)
         if torch.cuda.is_available():
-            trainer = pl.Trainer(max_epochs=50, accelerator="gpu", logger=wandb_logger)
+            trainer = pl.Trainer(max_epochs=10, accelerator="gpu", logger=wandb_logger)
         else:
             trainer = pl.Trainer(max_epochs=10, logger=wandb_logger)
         trainer.fit(edge_model, train_dataloaders=in_sample_edge_dataloader, val_dataloaders=out_sample_edge_dataloader)
@@ -103,3 +108,17 @@ def train():
 if __name__ == "__main__":
     sweep_id = wandb.sweep(sweep=sweep_configuration, project='note-predictor')
     wandb.agent(sweep_id, function=train)
+
+    # Uncomment (and comment the above two lines) to run a single training run
+    # config = {
+    #     'context_size': 10,
+    #     'hidden_dim': 128,
+    #     'link_lr': 0.01,
+    #     'lr': 0.0001,
+    #     'mlp_lr': 0.01,
+    #     'model_type': "attri2vec",
+    #     'num_layers': 3,
+    #     'output_dim': 256,
+    #     'walk_length': 10,
+    #  }
+    # train(args={'config': config, 'project': 'note-predictor'})
